6be16839fe2d45e32967bc45393e1c9547dd884d
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-11-25 15:53:09 +0600

    admin finding api created

--- index.js
+++ index.js
@@ -88,13 +88,21 @@ async function run() {
             res.send({ result })
         })
 
-
-        app.get('/users', verifyJWT, async (req, res) => {
+        // all users 
+        app.get('/users', async (req, res) => {
             const query = {}
             const users = await usersCollection.find(query).toArray();
             res.send(users)
         })
 
+
+        // admin users 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.accountType === 'admin' });
+        })
 
 
     }
